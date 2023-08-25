e49db7927d17eec95cfbba3d5f393f1553763dc0
Chang Min Bark <cb073@bucknell.edu>
2023-08-25 10:29:56 -0400

    lab01, exercise 1, complete

---
+++ src/main/java/lab01/Hello.java
@@ -0,0 +1,25 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Fall 2023
+* Instructor: Professor Stough
+* Section: 02 10AM
+* Lab Section: 10AM
+*
+* Name: Chang Min Bark
+* Date: 08/25/2023
+*
+* Lab / Assignment: lab01
+*
+* Description: A "Hello World" program in Java
+*
+* *****************************************/
+
+package lab01; // Note: Quick Fix options for initial package statement
+
+public class Hello
+{
+    public static void main( String[] args ){
+        System.out.println("Programming is not a spectator sport!");
+        System.exit(0);
+    }
+}
