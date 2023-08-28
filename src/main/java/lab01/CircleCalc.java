4761caedefb4bf912d012236c8cf0961ab7bd53a
Chang Min Bark <cb073@bucknell.edu>
2023-08-27 21:39:34 -0400

    lab01, exercise 4, complete

---
+++ src/main/java/lab01/CircleCalc.java
@@ -0,0 +1,13 @@
+package lab01;
+
+import java.util.Scanner;
+
+public class CircleCalc {
+    private static Scanner scnr;
+    private static final double EPSILON = 1.0E-10;
+    public static void main( String[] args ){
+        // Set up a common scanner to be used;
+        scnr = new Scanner()
+
+    }
+}
