3d40a2edaedff0b8f8e5903daceb0e8475e0e5be
Chang Min Bark <cb073@bucknell.edu>
2023-08-27 21:21:16 -0400

    lab01, exercise 3, complete

--- src/main/java/lab01/Hello.java
+++ src/main/java/lab01/Hello.java
@@ -14,12 +14,25 @@
 *
 * *****************************************/
 
+
 package lab01; // Note: Quick Fix options for initial package statement
+
+import java.util.Scanner;
 
 public class Hello
 {
     public static void main( String[] args ){
-        System.out.println("Programming is not a spectator sport!");
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String userName = scnr.next();
+        String osName = System.getProperty("os.name");
+        String osArch = System.getProperty("os.arch");
+        String osVer = System.getProperty("os.version");
+        System.out.println("Here is your operating system, " + userName);
+        System.out.println("OS Name: " + osName);
+        System.out.println("OS Architecture: " + osArch);
+        System.out.println("OS Version: " + osVer);
+        System.out.println(userName + ", your name is " + userName.length() + " characters long." );
         System.exit(0);
     }
 }
