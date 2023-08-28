2cbbcbd93365994369da90b963a6c5cd580a5175
Chang Min Bark <cb073@bucknell.edu>
2023-08-27 21:29:50 -0400

    lab01, exercise 4, complete

--- src/main/java/lab01/Hello.java
+++ src/main/java/lab01/Hello.java
@@ -25,6 +25,7 @@ public class Hello
         Scanner scnr = new Scanner(System.in);
         System.out.println("What is your name?");
         String userName = scnr.next();
+        // These assigns the operating system's properties to string variables.
         String osName = System.getProperty("os.name");
         String osArch = System.getProperty("os.arch");
         String osVer = System.getProperty("os.version");
@@ -33,6 +34,16 @@ public class Hello
         System.out.println("OS Architecture: " + osArch);
         System.out.println("OS Version: " + osVer);
         System.out.println(userName + ", your name is " + userName.length() + " characters long." );
+        System.out.println("What is your age?");
+        int age = scnr.nextInt();
+        // This if else block determines what to print based on the user's age.
+        if (age < 20) {
+            System.out.println("You're a teenager.");
+        } else if (age < 30) {
+            System.out.println("You're in your 20s.");
+        } else {
+            System.out.println("You're at least 30.");
+        }
         System.exit(0);
     }
 }
