ca724d49300fde262a0f800b7239897469bb2d47
Chang Min Bark <cb073@bucknell.edu>
2023-08-27 22:30:33 -0400

    lab01, exercise 5, complete

--- src/main/java/lab01/Hello.java
+++ src/main/java/lab01/Hello.java
@@ -21,8 +21,9 @@ import java.util.Scanner;
 
 public class Hello
 {
+    private static Scanner scnr;
     public static void main( String[] args ){
-        Scanner scnr = new Scanner(System.in);
+        scnr = new Scanner(System.in);
         System.out.println("What is your name?");
         String userName = scnr.next();
         // These assigns the operating system's properties to string variables.
