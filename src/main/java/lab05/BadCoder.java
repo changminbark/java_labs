fbb5a398aae78638429e0932dc9d896426dd3af6
Chang Min Bark <cb073@bucknell.edu>
2023-09-15 00:04:32 -0400

    lab05, exercise 2, refactored

--- src/main/java/lab05/BadCoder.java
+++ src/main/java/lab05/BadCoder.java
@@ -25,19 +25,43 @@ import java.util.Scanner;
 public class BadCoder {
 
     public static final int NUM_INTS = 10;
+    public static final int MAX_RANDOM_INT = 100;
 
     public static void main(String[] args) {
-        // Greet the user and ask for their name
-        Scanner scnr = new Scanner(System.in);
-        System.out.println("Greetings. What is your name?");
-        String name = scnr.next();
 
+        String name = greetUserAndGetName();
+        int[] x = generateArrayOfRandomInts(MAX_RANDOM_INT);
+
+        System.out.println(name + ", our array is: " + Arrays.toString(x));
+    }
+
+    /**
+     * Generate an array of random integers
+     *
+     * @param bound the maximum value int to randomly generate (exclusive, meaning all integers
+     *              from 0 (inclusive) to {@code bound} (exclusive).
+     * @return the array filled with integers
+     */
+    private static int[] generateArrayOfRandomInts(int bound) {
         // Let's fill up an array with random integers
         Random rand = new Random();
         int[] x = new int[NUM_INTS];
         for (int i = 0; i < NUM_INTS; i++){
-            x[i] = rand.nextInt(100);
+            x[i] = rand.nextInt(bound);
         }
-        System.out.println(name + ", our array is: " + Arrays.toString(x));
+        return x;
+    }
+
+    /**
+     * Greets user and asks them for their name as an input
+     *
+     * @return the string of their name
+     */
+    private static String greetUserAndGetName() {
+        // Greet the user and ask for their name
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Greetings. What is your name?");
+        String name = scnr.next();
+        return name;
     }
 }
