1d9d03d0cb0fba8ad2f3fa0ff6a8e209536dfc99
Chang Min Bark <cb073@bucknell.edu>
2023-09-26 16:31:14 -0400

    lab07, exercise 2, complete

--- src/main/java/lab07/SimpleCashRegister.java
+++ src/main/java/lab07/SimpleCashRegister.java
@@ -30,7 +30,23 @@ import java.util.List;
  *
  * @author Prof. Rick Zaccone and Brian King
  */
+
+/**
+ * A custom type of {@link Exception} used if the {@link SimpleCashRegister} tried
+ * to give chance before enough money is collected from the customer
+ */
+class ChangeException extends Exception {
+    public ChangeException(String message) {
+        super(message);
+    }
+}
+
 public class SimpleCashRegister {
+
+    /**
+     * Maximum price allowed on an item
+     */
+    private static final double MAX_ITEM_PRICE = 1000.0;
 
     /**
      * The total amount of the current transaction
@@ -88,8 +104,14 @@ public class SimpleCashRegister {
      * Records the sale of an item in a transaction.
      *
      * @param price the price of the item. Precondition: price >= 0
+     * @throws IllegalArgumentException if an invalid price is scanned
      */
     public void scanItem(double price) {
+        // Check for a valid price
+        if (price < 0.0 || price > MAX_ITEM_PRICE) {
+            String msg = String.format("scanItem: Bad Price: $%.2f", price);
+            throw new IllegalArgumentException(msg);
+        }
         // If this is the first purchase in the transaction, then clear out the
         // list of purchases
         if (totalTransaction == 0) {
@@ -106,8 +128,13 @@ public class SimpleCashRegister {
      *
      * @param moneyType the moneyType of the monetary units in the payment
      * @param unitCount the number of monetary units
+     * @throws IllegalArgumentException if unitCount is less than 0
      */
     public void collectPayment(Money moneyType, int unitCount) {
+        if (unitCount <= 0) {
+            String msg = String.format("scanItem: Bad number of units: %d", unitCount);
+            throw new IllegalArgumentException(msg);
+        }
         paymentCollected += unitCount * moneyType.getValue();
     }
 
@@ -116,8 +143,13 @@ public class SimpleCashRegister {
      * only if enough money was collected.
      *
      * @return the change due to the customer
+     * @throws ChangeException if insufficient payment was collected to cover the transaction
      */
-    public double giveChange() {
+    public double giveChange() throws ChangeException {
+        if (paymentCollected < totalTransaction) {
+            String msg = String.format("INSUFFICIENT PAYMENT Collected: $%.2f, transaction = $%.2f", paymentCollected, totalTransaction);
+            throw new ChangeException(msg);
+        }
         double change = paymentCollected - totalTransaction;
         totalTransaction = 0;
         paymentCollected = 0;
@@ -136,9 +168,19 @@ public class SimpleCashRegister {
         System.out.println("Payment made: " + myRegister.getPaymentCollected());
         System.out.println("Expected: 1.85");
 
-        double myChange = myRegister.giveChange();
-        System.out.println("Change: " + myChange);
-        System.out.println("Expected: 0.03");
+        try {
+            double myChange = myRegister.giveChange();
+            System.out.println("Change: " + myChange);
+            System.out.println("Expected: 0.03");
+        } catch (ChangeException e) {
+            System.err.println(e.getMessage());
+        }
+
+        // Check for an exception to bethrown for an invalid price scanned
+        // myRegister.scanItem(-0.50);
+
+        // Check for an exception to be thrown for a bad unitCount in collectPayment
+        // myRegister.collectPayment(Money.DIME, -1);
     }
 
 }
